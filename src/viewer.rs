aaed4ba54707c667433f16e16adcaae307b54150
Edu Renesto <edure95@gmail.com>
2022-12-13 22:41:18 -0300

    viewer: docs

--- src/viewer.rs
+++ src/viewer.rs
@@ -1,25 +1,49 @@
+//! # Visualizador
+//!
+//! Este arquivo contém a integração entre a parte de computação
+//! gráfica e a parte de cálculos.
+//!
+//! Não está inteiramente comentado porque não é o foco da disciplina.
+//!
+//! Mas, basicamente, esse arquivo contém a struct [`Viewer`], que configura
+//! o estado da placa de vídeo para a renderização, carrega a malha
+//! a partir do arquivo `.obj`, chama as funções de cálculo do módulo
+//! [`crate::geom`], e faz a renderização.
+
 use std::{io::BufReader, fs::File, f32::consts::PI};
 
 use ultraviolet::Vec3;
 
 use crate::gfx::{Shader, VertexBuffer};
 
+/// A struct `Viewer` armazena o estado da aplicação.
 pub struct Viewer<'a> {
+    /// Contexto OpenGL
     gl: &'a glow::Context,
 
+    /// O shader -- programa que é executado na placa de vídeo
+    /// para determinar a renderização
     shader: Shader,
+
+    /// Buffer de vértices, normais e curvaturas que é enviado
+    /// para a placa de vídeo
     vao: VertexBuffer,
 
+    /// Matriz que representa a transformação de espaço de objeto -> espaço de
+    /// tela para a projeção
     cam_matrix: ultraviolet::Mat4,
 }
 
 impl<'a> Viewer<'a> {
+    /// Constrói um novo `Viewer`.
     pub fn new(gl: &'a glow::Context) -> Viewer {
+        // Carrega o shader
         let shader = Shader::new(&gl,
             include_str!("../res/shaders/simple.frag.glsl"),
             include_str!("../res/shaders/simple.vert.glsl"),
         ).expect("failed to load shader");
 
+        // Carrega o modelo.
         let mut load_opts = tobj::GPU_LOAD_OPTIONS;
         load_opts.single_index = false;
 
@@ -28,19 +52,27 @@ impl<'a> Viewer<'a> {
             &load_opts,
         ).expect("failed to load model");
 
+        // Constrói o buffer.
+        // Aqui que chamamos as computações.
         let vao = {
             let model = &models[0];
             let mesh = &model.mesh;
 
+            // Calcula as vizinhanças.
             let nbhds = crate::geom::compute_neighborhoods(mesh);
+            // Calcula as normais médias.
             let raw_avg_normals = crate::geom::compute_avg_normals(mesh);
+            // Calcula as bases dos planos tangentes.
             let tangent_basii = crate::geom::compute_tangent_basis(mesh, &nbhds, &raw_avg_normals);
+            // Calcula as matrizes dos shape operators.
             let shape_ops = crate::geom::compute_shape_operator(mesh, &nbhds, &tangent_basii);
+            // Calcula as curvaturas gaussianas e normais.
             let raw_curvatures = crate::geom::compute_curvatures(&shape_ops);
 
             let raw_positions = &mesh.positions;
             let raw_normals = &mesh.normals;
 
+            // Prepara os vértices de um modo que a placa de vídeo espera.
             let vertices = mesh
                 .indices
                 .chunks_exact(3)
@@ -66,6 +98,7 @@ impl<'a> Viewer<'a> {
                 .flatten()
                 .collect::<Vec<_>>();
 
+            // Prepara as normais de um modo que a placa de vídeo espera.
             let normals = mesh
                 .normal_indices
                 .chunks_exact(3)
@@ -91,6 +124,7 @@ impl<'a> Viewer<'a> {
                 .flatten()
                 .collect::<Vec<_>>();
 
+            // Prepara as normais médias de um modo que a placa de vídeo espera.
             let avg_normals = mesh
                 .indices
                 .chunks_exact(3)
@@ -104,6 +138,7 @@ impl<'a> Viewer<'a> {
                 .flatten()
                 .collect::<Vec<_>>();
 
+            // Prepara as curvaturas gaussianas de um modo que a placa de vídeo espera.
             let curvatures = mesh
                 .indices
                 .chunks_exact(3)
@@ -120,12 +155,31 @@ impl<'a> Viewer<'a> {
             println!("raw_curvatures.len = {}, raw_vertices.len = {}", raw_curvatures.len(), raw_positions.len());
 
             //VertexBuffer::from_mesh(gl, vertices, Some(normals), None)
+            // Constrói o buffer e copia os dados para a placa de vídeo.
             VertexBuffer::from_mesh(gl, vertices, Some(avg_normals), Some(curvatures))
         };
 
+        // Constrói a matriz da transformação da projeção.
         let cam_matrix = {
-            let view = ultraviolet::Mat4::look_at(Vec3::new(2.0, 2.0, 2.0), Vec3::zero(), Vec3::new(0.0, 1.0, 0.0));
-            let projection = ultraviolet::projection::perspective_gl(PI / 3.0, 16.0/9.0, 1.0, 100.0);
+            let view = ultraviolet::Mat4::look_at(
+                // Observador está no ponto (2, 2, 2) ...
+                Vec3::new(2.0, 2.0, 2.0),
+                // ... olhando para o ponto (0, 0, 0) ...
+                Vec3::zero(),
+                // ... e o topo de sua cabeça aponta para (0, 1, 0).
+                Vec3::new(0.0, 1.0, 0.0),
+            );
+
+            let projection = ultraviolet::projection::perspective_gl(
+                // Campo de visão de PI / 3...
+                PI / 3.0,
+                // ... com aspecto de 16/9 (widescreen) ...
+                16.0/9.0,
+                // ... e considerando tudo numa distância entre 1.0 ...
+                1.0,
+                // ... e 100.0.
+                100.0,
+            );
 
             projection * view
         };
@@ -138,6 +192,7 @@ impl<'a> Viewer<'a> {
         }
     }
 
+    /// Renderiza a cena.
     pub fn render(&self) {
         self.shader.bind(self.gl);
         self.shader.uniform(self.gl, "_camera_mtx", &self.cam_matrix);
